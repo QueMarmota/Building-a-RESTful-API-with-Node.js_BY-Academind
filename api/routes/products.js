31a1de10a97a985cbc5a753e4c188b7cf9004aa6
Rafael Alejandro Renteria Gomez <rarg93@hotmail..com>
2018-10-27 14:20:59 -0500

    Se realizo la conexion con mongodbATLAS y se implemento el schema para products dando asi por termiando los recursos de la ruta recursos PUT DELETE POST GET GET/ID

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,51 +1,111 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message: 'Manejador de peticiones GET /products'
-    });
+const Product = require('../models/product');//archivo donde obtenemos el schema de producto
+
+router.get('/', (req, res, next) => {
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            //El codigo dle if es para si no existen datos
+            // if (docs.length >= 0) {
+            res.status(200).json(docs);
+            // }else{
+            //     res.status(404).json({
+            //         message:'No hay datos'
+            //     });
+            // }
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
 });
 
-router.post('/',(req,res,next)=>{
-    console.log(req.body);
-    const product = {
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Manejador de peticiones POST /products',
-        createdProduct:product
     });
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Manejador de peticiones POST /products',
+            createdProduct: product
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+
+
 });
 
-router.get('/:productId',(req,res,next)=>{
+router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if(id === 'special')
-    {
-        res.status(200).json({
-            message: 'Encontraste el id especial',
-            id: id
-        });    
-    }
-    else{
-        res.status(200).json({
-            message: 'Pasaste un ID'
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log('From database', doc);//si todo salio bn
+            if (doc)//si el id existe
+            {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: 'No valid entry found for provided ID' });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err });
         });
+})
+
+router.put('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    //Este ciclo nos ayuda para checar que campos se cambiaron y si no se modificaron campos se queda con los actuales
+    //evitandonos el tener que validar si solo quiere actualizar el nombre o el precio y despues hacer la actualizacion
+    //pero con este ajuste cambia la forma en como hacemos el put en el body , puesto que hay q pasarle un arreglo con el propName
+    // [
+	// {"propName":"name","value":"Leche"}
+    // ]
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
     }
+    Product.update({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            err.status(500).json({
+                error: err
+            });
+        });
 })
 
-router.put('/:productId',(req,res,next)=>{
+router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'products actualizada'
-    });
-})
-
-router.delete('/:productId',(req,res,next)=>{
-    const id = req.params.productId;
-    res.status(200).json({
-        message: 'products eliminada'
-    });
+    Product.remove({
+        _id: id
+    }).exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 module.exports = router;
--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');//morgan nos regresa el resultado y tiempo de las peticiones
 const bodyParser = require('body-parser');//body parser se utiliza para cuando hacemos post con json para traer los datos con req.body
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products')//rutas que nos llevan a nuestros recursos
 const orderRoutes = require('./api/routes/orders')
-//morgan
+//mongoose
+//la cadena de conexion se obtiene de la paginad e mongo despues de haber creado un cluster con atlas y MONGO_ATLAS_PW es la contraseña que definimos para el usuario del cluster
+//y esa pw se encuentra en nodemon.json para no tener que ponerla en la cadena de conexion(la contraseña no debe tener caracteres especiales).
+mongoose.connect("mongodb://node-shop:"+
+    process.env.MONGO_ATLAS_PW+
+    "@node-rest-shop-practica-shard-00-00-uw98x.mongodb.net:27017,node-rest-shop-practica-shard-00-01-uw98x.mongodb.net:27017,node-rest-shop-practica-shard-00-02-uw98x.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-practica-shard-0&authSource=admin&retryWrites=true"
+    ,{ useNewUrlParser: true }
+    
+);
+//morgan 
 app.use(morgan('dev'));
 //BODY PARSER
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,6 +37,7 @@ app.use((req,res,next)=>{
 //rutas que deben manejar una peticion
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+
 
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
